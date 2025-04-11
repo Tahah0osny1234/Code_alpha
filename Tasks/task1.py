874530c689472ddc610cc626c7d69c6452a702d3
Tahah0osny1234 <tahah0665@gmail.com>
2025-04-11 10:56:54 +0200

    Update task1.py

--- Tasks/task1.py
+++ Tasks/task1.py
@@ -1,1 +1,17 @@
 
+def fib(num):
+    a=0
+    b=1
+    for _ in range(num):
+        print(a, end=' ')
+        temp = a
+        a = b
+        b = temp + b
+
+
+
+
+x = int(input("Enter the number of Fibonacci numbers you want to print: "))
+fib(x)
+
+#done task 1
